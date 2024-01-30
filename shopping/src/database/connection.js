cdf8753d47d17df0fe238ce8ee3f5570ab0b42c7
Nikhil <nkarnwal07@gmail.com>
2024-01-30 14:47:33 +0530

    bug fixed

--- shopping/src/database/connection.js
+++ shopping/src/database/connection.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const { DB_URL } = require("../config");
 
 module.exports = async () => {
-    console.log(DB_URL)
+  console.log(DB_URL);
   try {
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Db Connected");
   } catch (error) {
     console.log("Error ============");
